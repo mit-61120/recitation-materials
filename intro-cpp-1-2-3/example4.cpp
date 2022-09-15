d9934a67f9747b4e46d292cfcb76ec6a223be805
Luka Govedic <luka.govedic@gmail.com>
2022-09-15 18:07:44 -0400

    Fixed example4

--- intro-cpp-1-2-3/example4.cpp
+++ intro-cpp-1-2-3/example4.cpp
@@ -12,7 +12,7 @@
 #include <string>
 #include <algorithm>
 
-void birthday2(User user) {
+void birthday2(User &user) {
     user.age;
 }
 /*
@@ -45,4 +45,14 @@ int main() {
     std::cout << users[1].age << std::endl;
 
     // TODO find all users whose names start with L or M (inclusive) and output them
+    std::vector<User> lmUsers;
+    std::copy_if(users.begin(), users.end(), std::back_inserter(lmUsers), [](const User &u) -> bool {
+        return u.name[0] == 'L' || u.name[0] == 'M';
+    });
+
+    std::sort(users.begin(), users.end());
+    std::vector lmUsers2(std::lower_bound(users.begin(), users.end(), User{"L", 0}), std::upper_bound(users.begin(), users.end(), User{"M"}));
+
+    std::cout << lmUsers.size() << std::endl;
+    std::cout << (users2[0] < users2[1]) << std::endl;
 }
