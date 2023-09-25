16ea25565a027ef993e55d10d8c3444badd12842
Charles Yuan <charlesyuan314@gmail.com>
2023-09-25 11:06:03 -0400

    Reformat

--- rec3/util/Visitor.h
+++ rec3/util/Visitor.h
@@ -3,21 +3,20 @@
 #include <tuple>
 
 namespace util {
-    namespace detail {
-        template<typename T>
-        class VisitorOfOne {
-        public:
-            virtual void visit(T const &) = 0;
-        };
+namespace detail {
+template <typename T>
+class VisitorOfOne {
+ public:
+  virtual void visit(T const &) = 0;
+};
 
+}  // namespace detail
 
-    }
+template <typename... Ts>
+class Visitor : detail::VisitorOfOne<Ts>... {
+ public:
+  using Types = std::tuple<Ts...>;
 
-    template<typename ...Ts>
-    class Visitor : detail::VisitorOfOne<Ts> ... {
-    public:
-        using Types = std::tuple<Ts...>;
-
-        using detail::VisitorOfOne<Ts>::visit...;
-    };
-}
+  using detail::VisitorOfOne<Ts>::visit...;
+};
+}  // namespace util
